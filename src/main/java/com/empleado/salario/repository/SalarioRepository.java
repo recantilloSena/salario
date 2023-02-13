0760fe668160cb240f2790643856df5e37caeb4c
Ricardo Enrique Cantillo Carrillo <recantillo1@misena.edu.co>
2023-02-13 16:33:00 -0500

    Incluye Capa de Repository
    Incluye una Capa más de operaciones con datos o Repository

---
+++ src/main/java/com/empleado/salario/repository/SalarioRepository.java
@@ -0,0 +1,44 @@
+package com.empleado.salario.repository;
+
+import com.empleado.salario.modelo.Empleado;
+
+public class SalarioRepository {
+	
+	public Empleado createEmpleado(String cedula, Integer tipo, Integer salario) {
+		Empleado e = new Empleado();
+		e.setCedula(cedula);
+		e.setTipo(tipo);
+		e.setSalario(salario);
+		e.setPago(getSalarioFinal(
+				                  e.getSalario(),
+				                  e.getTipo())
+				   );		
+		
+		return e;
+	}
+	
+public float getSalarioFinal(Integer salarioBase, Integer tipo) {
+		
+		float salarioFinal=(float) 0.0;		
+		switch (tipo) { 
+	    case 1:
+	    	salarioFinal = salarioBase + salarioBase*3/100;
+	     break;
+	    case 2:
+	    	salarioFinal = salarioBase + salarioBase*5/100;
+	     break;
+	    case 3:
+	    	salarioFinal = salarioBase + salarioBase*10/100;
+	     break;  
+	    case 4:
+	    	salarioFinal = salarioBase + salarioBase*15/100;
+	     break;   	     
+	    default:	    
+	    	salarioFinal = salarioBase ;
+	     break;	     
+	  }	
+		return salarioFinal;
+		
+	}
+
+}
--- src/main/java/com/empleado/salario/service/SalarioService.java
+++ src/main/java/com/empleado/salario/service/SalarioService.java
@@ -3,12 +3,17 @@ package com.empleado.salario.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.empleado.salario.modelo.Empleado;
+import com.empleado.salario.repository.SalarioRepository;
 
 @Service
 public class SalarioService {
+	
+	@Autowired
+	SalarioRepository salarioRepository;
 	
 	public Integer contar(String palabra) {		
 		return palabra.length();
@@ -18,56 +23,25 @@ public class SalarioService {
 		return n1 + n2;
 	}
 	
-	public float getSalarioFinal(Integer salarioBase, Integer tipo) {
-		
-		float salarioFinal=(float) 0.0;		
-		switch (tipo) { 
-	    case 1:
-	    	salarioFinal = salarioBase + salarioBase*3/100;
-	     break;
-	    case 2:
-	    	salarioFinal = salarioBase + salarioBase*5/100;
-	     break;
-	    case 3:
-	    	salarioFinal = salarioBase + salarioBase*10/100;
-	     break;  
-	    case 4:
-	    	salarioFinal = salarioBase + salarioBase*15/100;
-	     break;   	     
-	    default:	    
-	    	salarioFinal = salarioBase ;
-	     break;	     
-	  }	
-		return salarioFinal;
-		
-	}
+	
 
 		
 	public List<Empleado> getEmpleados(){
-		List<Empleado> lista= new ArrayList();		
-		Empleado e1 = new Empleado();
-		e1.setCedula("e1");
-		e1.setTipo(4);
-		e1.setSalario(500000);
-		e1.setPago(getSalarioFinal(
-				                  e1.getSalario(),
-				                  e1.getTipo())
-				   );		
-		lista.add(e1);
+		List<Empleado> lista= new ArrayList<Empleado>();
 		
-		Empleado e2 = new Empleado();
-		e2.setCedula("e2");
-		e2.setTipo(1);
-		e2.setSalario(600000);
-		e2.setPago(getSalarioFinal(
-				                  e2.getSalario(),
-				                  e2.getTipo())
-				   );		
-		lista.add(e2);	
-	
+		lista.add(salarioRepository.createEmpleado("E1", 1, 200000));
+		lista.add(salarioRepository.createEmpleado("E2", 4, 500000));
+			
 		
 		return lista;
 	}
+
+	public float getSalarioFinal(Integer salario, Integer tipo) {		
+		return salarioRepository.getSalarioFinal(salario, tipo);
+	}
+	
+	
+	
 	
 	
 	
